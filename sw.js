498430dc5374c18e3f479433bfe24a695e9464b8
INGELAR <whmhoff@gmail.com>
2026-09-08 08:33:00 -0300

    Dashboard solar 2026-09-08

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'solar-202609070833';
+const CACHE = 'solar-202609080833';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(
